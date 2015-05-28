25fb220eb219413bfc79e99cb27e3897fe1b63f1
Jon Skulski <jskulski@gmail.com>
2015-05-27 17:07:28 -0700

    First acceptance test that creates Basic Edit.
    ===
    Right now running the test 'suite' will launch a GUI. That's a design
    that will probably have to change, but for now let's march on.
    
    The goal is to find a way to test that PyRoom is creating a 'session' to
    keep track of all open buffers so that on reload it reloads that
    buffer.

--- test/test_session_acceptance.py
+++ test/test_session_acceptance.py
@@ -1,8 +1,21 @@
+import sys
+sys.path.append('../PyRoom')
+
+import gettext
+
 import unittest
 
+import __builtin__
+__builtin__._ = lambda str: str
+
+from basic_edit import BasicEdit
+from preferences import PyroomConfig
 
 class SessionAcceptanceTest(unittest.TestCase):
 
     def test_testing_framework_is_setup(self):
         self.assertEqual(True, True)
 
+    def test_create_an_editor_object(self):
+        pyroom_config = PyroomConfig()
+        base_edit = BasicEdit(pyroom_config)
