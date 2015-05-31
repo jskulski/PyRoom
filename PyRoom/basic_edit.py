001e5921ac5631ec2ea67b2ccdb113dabf4f1ddb
Jon Skulski <jskulski@gmail.com>
2015-05-31 12:30:02 -0700

    Fixing regular mode and adding tests to make sure we don't affect normal mode

--- PyRoom/basic_edit.py
+++ PyRoom/basic_edit.py
@@ -249,7 +249,7 @@ class BasicEdit(object):
                 self.keybindings[event.hardware_keycode]()
                 return True
 
-        if self.vim_emulator.in_command_mode() and event.keyval & gtk.keysyms.i:
+        if self.vim_emulator and self.vim_emulator.in_command_mode() and event.keyval & gtk.keysyms.i:
             self.vim_emulator.toggle_mode()
             return True
 
